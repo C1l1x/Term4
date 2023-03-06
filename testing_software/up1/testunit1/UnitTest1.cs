2a33d08bda383ef1e47965da10898fb364e9ff2c
C1l1x <pavlov_s@abv.bg>
2023-03-06 17:34:08 +0200

    init

---
+++ testing_software/up1/testunit1/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace testunit1;
+using tictactoe;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestGameCurrentPlayer()
+    {
+        Class game = new Class();
+
+        int expectedPlayer = 1;
+        int actualPlayer = game.getCurrentPlayer();
+
+        Assert.AreEqual(expectedPlayer, actualPlayer);
+    }
+}
